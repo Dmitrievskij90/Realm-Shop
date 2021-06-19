db1172083aaf126350fc3e97358e3eb9b0e6086e
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-06-19 22:08:59 +0600

    update constraints for categoryCollectionView

--- Realm Shop/Controllers/CategoryViewController/CategoryViewController.swift
+++ Realm Shop/Controllers/CategoryViewController/CategoryViewController.swift
@@ -8,8 +8,10 @@
 import UIKit
 
 class CategoryViewController: UIViewController {
+    private let leftInset: CGFloat = 5
+    private let topInset: CGFloat = 0
     @IBOutlet weak var categoryCollectionView: UICollectionView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButonPressed))
@@ -32,14 +34,25 @@ class CategoryViewController: UIViewController {
 
 extension CategoryViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 6
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCollectionViewCell.identifier, for: indexPath) as? CategoryCollectionViewCell else {
             return UICollectionViewCell()
         }
-
+        cell.layer.cornerRadius = 10
         return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (view.frame.width / 3) - 10, height: (view.frame.width / 3) - 10)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: topInset, left: leftInset, bottom: topInset, right: leftInset)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     }
 }
