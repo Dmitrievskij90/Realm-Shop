a88fe588051cc65237cb77c0ee2406719d011c1a
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-06-19 21:39:39 +0600

    add  + button to CategoryViewController

--- Realm Shop/Controllers/CategoryViewController/CategoryViewController.swift
+++ Realm Shop/Controllers/CategoryViewController/CategoryViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 class CategoryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButonPressed))
+        navigationItem.rightBarButtonItem = addButton
+        navigationController?.navigationBar.tintColor = .label
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.isHidden = false
+    }
+
+    @objc func addButonPressed() {
     }
 }
