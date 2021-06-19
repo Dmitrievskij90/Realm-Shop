f5496d27ce55a616a420fb60d78f8d53eae7fe21
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-06-20 00:56:12 +0600

    add + button

--- Realm Shop/Controllers/ItemViewController/ItemViewController.swift
+++ Realm Shop/Controllers/ItemViewController/ItemViewController.swift
@@ -5,10 +5,26 @@
 //  Created by Konstantin Dmitrievskiy on 20.06.2021.
 //
 
+import RealmSwift
 import UIKit
 
 class ItemViewController: UIViewController {
+    var todoItems: Results<Item>?
+
+    let realm = try! Realm()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButonPressed))
+        navigationItem.rightBarButtonItem = addButton
+        navigationController?.navigationBar.tintColor = .label
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    @objc func addButonPressed() {
+    }
+
 }
