e90900da50db7a9687758b178bb08d95ff971338
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-06-20 00:27:42 +0600

    add ItemViewController

---
+++ Realm Shop/Controllers/ItemViewController/ItemViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ItemViewController.swift
+//  Realm Shop
+//
+//  Created by Konstantin Dmitrievskiy on 20.06.2021.
+//
+
+import UIKit
+
+class ItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
