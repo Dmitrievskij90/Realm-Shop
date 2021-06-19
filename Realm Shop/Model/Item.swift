9cf755fd4d6fe72776e34d4f096bb6a73ddcd9bc
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-06-20 00:36:28 +0600

    add Item model

---
+++ Realm Shop/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Realm Shop
+//
+//  Created by Konstantin Dmitrievskiy on 20.06.2021.
+//
+
+import Foundation
+import RealmSwift
+
+class Item: Object {
+    @objc dynamic var title: String = ""
+    var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
+}
