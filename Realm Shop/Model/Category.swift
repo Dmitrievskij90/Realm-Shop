6da8f19f677c3c1fc49c51269d38613db7234c2c
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-06-25 22:04:48 +0600

    add colour to Category model

--- Realm Shop/Model/Category.swift
+++ Realm Shop/Model/Category.swift
@@ -10,5 +10,7 @@ import RealmSwift
 
 class Category: Object {
     @objc dynamic var name: String = ""
+    @objc dynamic var colour: Int = 0
     let items = List<Item>()
+    let categoryBackground = [0x94D0CC, 0xEEC4C4, 0x9FE6A0, 0xF1CA89, 0xCE97B0, 0x907FA4, 0xF7A440, 0xFFCC29]
 }
