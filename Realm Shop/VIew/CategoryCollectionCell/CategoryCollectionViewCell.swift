edefb2e35dd59d465ffcd58385476bc67081442b
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-07-28 22:33:25 +0600

    add animation methods to CategoryCollectionViewCell

--- Realm Shop/VIew/CategoryCollectionCell/CategoryCollectionViewCell.swift
+++ Realm Shop/VIew/CategoryCollectionCell/CategoryCollectionViewCell.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class CategoryCollectionViewCell: UICollectionViewCell {
     static let identifier = "CategoryCollectionViewCell"
+    private var isAnimate: Bool! = true
+
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var removeButton: UIButton!
 
@@ -19,5 +21,34 @@ class CategoryCollectionViewCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         categoryLabel.font = UIFont(name: "Marker felt", size: 20)
+    }
+
+    func startAnimate() {
+        let shakeAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        shakeAnimation.duration = 0.05
+        shakeAnimation.repeatCount = 4
+        shakeAnimation.autoreverses = true
+        shakeAnimation.duration = 0.2
+        shakeAnimation.repeatCount = 99_999
+
+        let startAngle: Float = (-2) * 3.14 / 180
+        let stopAngle = -startAngle
+
+        shakeAnimation.fromValue = NSNumber(value: startAngle as Float)
+        shakeAnimation.toValue = NSNumber(value: 3 * stopAngle as Float)
+        shakeAnimation.autoreverses = true
+        shakeAnimation.timeOffset = 290 * Double.random(in: 0.0...1.0)
+
+        let layer: CALayer = self.layer
+        layer.add(shakeAnimation, forKey: "animate")
+        removeButton.isHidden = false
+        isAnimate = true
+    }
+
+    func stopAnimate() {
+        let layer: CALayer = self.layer
+        layer.removeAnimation(forKey: "animate")
+        self.removeButton.isHidden = true
+        isAnimate = false
     }
 }
