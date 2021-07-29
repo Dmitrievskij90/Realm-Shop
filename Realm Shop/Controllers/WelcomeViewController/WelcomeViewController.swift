590e640b1725f2d489b2874f6c1e193eff2a866b
Konstantin Dmitrievskiy <dmitrievskij90@mail.ru>
2021-07-29 23:16:00 +0600

    use presenter for update WelcomeViewController

--- Realm Shop/Controllers/WelcomeViewController/WelcomeViewController.swift
+++ Realm Shop/Controllers/WelcomeViewController/WelcomeViewController.swift
@@ -8,23 +8,21 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
+    lazy var presenter = WelcomeScreenPresenter(view: self)
+    
     @IBOutlet weak var getStartedButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        getStartedButton.layer.cornerRadius = 10
-        getStartedButton.backgroundColor = .init(hex: 0xF4D085)
+        presenter.viewDidLoad(getStartedButton)
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.navigationBar.isHidden = true
+        presenter.viewWillAppear(navigationController)
     }
 
     @IBAction private func getStartedButtonPressed(_ sender: UIButton) {
-        let viewController = CategoryViewController.instantiate()
-        viewController.modalTransitionStyle = .coverVertical
-        viewController.modalPresentationStyle = .fullScreen
-        navigationController?.pushViewController(viewController, animated: true)
+        presenter.getStartedButtonPressed(navigationController)
     }
 }
